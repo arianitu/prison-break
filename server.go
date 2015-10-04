b897991912544e6bdec7051babda6b05a693443a
Arianit Uka <arianitu@gmail.com>
2015-10-04 15:02:10 -0400

    Switch to gorilla websockets and use maps for connections

--- server.go
+++ server.go
@@ -5,35 +5,72 @@ import (
 	"flag"
 	"fmt"
 	"github.com/tarm/serial"
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
+	"sync"
 	"log"
 	"net/http"
 	"time"
 )
 
-type Client struct {
-	ws *websocket.Conn
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
 }
 
 type Server struct {
-	conns []*websocket.Conn
+	conns map[*websocket.Conn]bool
+	rwl *sync.RWMutex
 }
 
 func (s *Server) init() {
-	s.conns = make([]*websocket.Conn, 0)
+	s.conns = make(map[*websocket.Conn]bool);
+	s.rwl = &sync.RWMutex{}
 }
 
-func (s *Server) handleClientConnection(conn *websocket.Conn) {
-	s.conns = append(s.conns, conn)
+func (s *Server) handleClientConnection(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got connection!")
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	s.rwl.Lock()
+	s.conns[ws] = true
+	s.rwl.Unlock()
+	
+	for {
+		_, _, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+	}
+	
+	log.Println("Removed connection!")
+	s.rwl.Lock()
+	delete(s.conns, ws)
+	s.rwl.Unlock()
 }
 
 func (s *Server) broadcast(message string) {
-	for _, conn := range s.conns {
-		websocket.Message.Send(conn, message)
+	s.rwl.RLock()
+	for conn, _ := range s.conns {
+		conn.WriteMessage(websocket.TextMessage, []byte(message))
 	}
+	s.rwl.RUnlock()
 }
 
 func (s *Server) setupSerialProxy(c *serial.Config) {
+	return
+	
 	p, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
@@ -66,12 +103,14 @@ func main() {
 	}
 
 	server := &Server{}
+	server.init()
+	
 	c := &serial.Config{Name: *serialPort, Baud: *baudRate}
 	go server.setupSerialProxy(c)
 	go server.setupKeepAlive()
 
 	log.Printf("Proxying from serial port %s to websocket url %s:%d%s \n", *serialPort, *httpHost, *httpPort, *websocketUrl)
-	http.Handle(*websocketUrl, websocket.Handler(server.handleClientConnection))
+	http.HandleFunc(*websocketUrl, server.handleClientConnection)
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *httpHost, *httpPort), nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
