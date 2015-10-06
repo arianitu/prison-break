2367ee35fe320cd7afceeb97afc8f1a44cf238cb
Arianit Uka <arianitu@gmail.com>
2015-10-05 21:51:53 -0400

    Make writes thread safe and use control messages for keep-alive. Also added a test function to broadcast mock commands

--- server.go
+++ server.go
@@ -10,8 +10,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"io"
 )
-
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
@@ -21,13 +21,19 @@ var upgrader = websocket.Upgrader{
     },
 }
 
+type Conn struct {
+	Mutex *sync.Mutex
+	Ws *websocket.Conn
+}
+
 type Server struct {
-	conns map[*websocket.Conn]bool
+	From io.Reader
+	conns map[*Conn]bool
 	rwl *sync.RWMutex
 }
 
 func (s *Server) init() {
-	s.conns = make(map[*websocket.Conn]bool);
+	s.conns = make(map[*Conn]bool);
 	s.rwl = &sync.RWMutex{}
 }
 
@@ -42,50 +48,65 @@ func (s *Server) handleClientConnection(w http.ResponseWriter, r *http.Request) 
 		log.Println(err)
 		return
 	}
-
+	
+	conn := &Conn{ Ws: ws, Mutex: &sync.Mutex{}}
 	s.rwl.Lock()
-	s.conns[ws] = true
+	s.conns[conn] = true
 	s.rwl.Unlock()
 	
 	for {
 		_, _, err := ws.ReadMessage()
 		if err != nil {
+			log.Println(err)
 			break
 		}
 	}
 	
 	log.Println("Removed connection!")
 	s.rwl.Lock()
-	delete(s.conns, ws)
+	delete(s.conns, conn)
 	s.rwl.Unlock()
 }
 
 func (s *Server) broadcast(message string) {
 	s.rwl.RLock()
 	for conn, _ := range s.conns {
-		conn.WriteMessage(websocket.TextMessage, []byte(message))
+		conn.Mutex.Lock()
+		log.Println("Broadcasting: " + message)
+		if err := conn.Ws.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+			log.Println(err)
+		}
+		conn.Mutex.Unlock()
 	}
 	s.rwl.RUnlock()
 }
 
-func (s *Server) setupSerialProxy(c *serial.Config) {
-	return
-	
-	p, err := serial.OpenPort(c)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	scanner := bufio.NewScanner(p)
+func (s *Server) proxy() {
+	time.Sleep(5 * time.Second)
+	scanner := bufio.NewScanner(s.From)
 	for scanner.Scan() {
 		s.broadcast(scanner.Text())
 	}
+	fmt.Println("Done")
 }
 
 func (s *Server) setupKeepAlive() {
 	ticker := time.NewTicker(5 * time.Second)
 	for _ = range ticker.C {
-		s.broadcast("KEEP-ALIVE")
+		for conn, _ := range s.conns {
+			conn.Ws.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(30 * time.Second))
+		}
+	}
+}
+
+func testCommands(p *io.PipeWriter) {
+	commands := []string{"k2110224\n", "k2110225\n", "k2110226\n"}
+	ticker := time.NewTicker(5 * time.Second)
+	
+	i := 0
+	for _ = range ticker.C {
+		io.WriteString(p, commands[i % len(commands)])
+		i++
 	}
 }
 
@@ -102,13 +123,24 @@ func main() {
 		log.Fatalln("Serial port is required! Use --help for more info")
 	}
 
-	server := &Server{}
+	test := "lol"
+	fmt.Println([]byte(test))
+	
+	pr, pw := io.Pipe()
+	server := &Server{From: pr}
 	server.init()
 	
 	c := &serial.Config{Name: *serialPort, Baud: *baudRate}
-	go server.setupSerialProxy(c)
-	go server.setupKeepAlive()
+	fmt.Println(c)
+	// p, err := serial.OpenPort(c)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
+	go server.setupKeepAlive()
+	go server.proxy()
+	go testCommands(pw)
+	
 	log.Printf("Proxying from serial port %s to websocket url %s:%d%s \n", *serialPort, *httpHost, *httpPort, *websocketUrl)
 	http.HandleFunc(*websocketUrl, server.handleClientConnection)
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *httpHost, *httpPort), nil)
@@ -116,3 +148,5 @@ func main() {
 		panic("ListenAndServe: " + err.Error())
 	}
 }
+
+
